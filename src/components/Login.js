429dec6e7ac3c92f27b00106e07535603c5598a6
sachin <ysachin511@gmail.com>
2022-11-30 23:05:20 +0530

    update protocol

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const navigate=useNavigate()
  
     const google = () => {
-    window.open("https://capstone-6pkp.onrender.com/auth/google", "_self");
+    window.open("http://capstone-6pkp.onrender.com/auth/google", "_self");
     
   };
   const github = () => {
@@ -30,7 +30,7 @@ const Login = () => {
 
   const handleLogin=async()=>{
     setToggle(true)
-    let res=await axios.post("https://capstone-6pkp.onrender.com/users/signin",{
+    let res=await axios.post("http://capstone-6pkp.onrender.com/users/signin",{
       email,password
     
     })
