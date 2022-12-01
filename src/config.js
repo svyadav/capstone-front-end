f7fd8c342acdf6aaad8446dd62076f88cd631f14
Sachin Yadav <ysachin511@gmail.com>
2022-12-01 15:48:15 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const serverUrl = 'http://localhost:5000'
+// const serverUrl = 'http://localhost:5000'
+const serverUrl = 'https://capstone-back-end.onrender.com'
+
 
 module.exports = {
     serverUrl
